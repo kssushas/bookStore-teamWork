bab82c4ac4310cd64b73e66ba474d822a9ac8322
Igor Konoval <kip_y@sfa.org.ua>
2023-10-16 02:50:52 +0300

    edit settings

--- src/main.js
+++ src/main.js
@@ -1,11 +1,8 @@
+import './js/allcategories.js';
 import './js/anchor-btn';
+import './js/apisearch.js';
+import './js/header.js';
 import './js/loader';
-import './js/header.js';
-import './js/allcategories.js';
 import './js/modal.js';
+// import './js/shoping-card.js';
 import './js/support.js';
-import './js/header.js';
-import './js/allcategories.js';
-import './js/modal.js';
-import './js/support.js';
-import './js/shoping-card.js';
