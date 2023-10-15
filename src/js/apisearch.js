9dbe7b69f57556548cf007b99a77bc2d60fed046
OksanaKorsun <oksana2081989@gmail.com>
2023-10-16 00:42:55 +0300

    import

--- src/js/apisearch.js
+++ src/js/apisearch.js
@@ -2,67 +2,75 @@ import axios from 'axios';
 
 axios.defaults.baseURL = 'https://books-backend.p.goit.global/books/';
 
-async function makeList() {
-  const result = axios.get('top-books');
-  const resultVal = await result.then(data => data.data);
-  const dta = await resultVal.map(val => val.list_name);
-  console.log(dta)
-  const markupLi = markup(dta);
-  list.insertAdjacentHTML('beforeend', markupLi);
+//  Не чіпати - це запит для модалки!!!
+export async function serviceModal(touchId) {
+  const responce = await axios.get(`${touchId}`);
+  return await responce.data; 
 }
+// !!!!!!
+// В цьому файлі мають бути лише запити!!!! Такий як зверху!!! Без викликів функцій, без розмітки
+
+// async function makeList() {
+//   const result = axios.get('top-books');
+//   const resultVal = await result.then(data => data.data);
+//   const dta = await resultVal.map(val => val.list_name);
+//   console.log(dta)
+//   const markupLi = markup(dta);
+//   list.insertAdjacentHTML('beforeend', markupLi);
+// }
 
 
-makeList();
+// makeList();
 
-// // ------------------------------------------------------------------ //
+// // // ------------------------------------------------------------------ //
 
-const listOfBookFromCategory = document.querySelector(
-  '.listOfBookFromCategory'
-);
+// const listOfBookFromCategory = document.querySelector(
+//   '.listOfBookFromCategory'
+// );
 
-list.addEventListener('click', e => {
-  const nameOfCategory = e.target.textContent;
+// list.addEventListener('click', e => {
+//   const nameOfCategory = e.target.textContent;
 
-  async function makeRequest() {
-    const categoryList = axios.get(`category?category=${nameOfCategory}`);
-    const resReq = await categoryList.then(data => data.data);
-    const markupListBook = markupBookOfcategory(resReq);
-    listOfBookFromCategory.innerHTML = markupListBook;
-  }
+//   async function makeRequest() {
+//     const categoryList = axios.get(`category?category=${nameOfCategory}`);
+//     const resReq = await categoryList.then(data => data.data);
+//     const markupListBook = markupBookOfcategory(resReq);
+//     listOfBookFromCategory.innerHTML = markupListBook;
+//   }
 
-  makeRequest();
-});
+//   makeRequest();
+// });
 
 // //-----------------------------------------------------//
 
-async function topFive() {
-  const result1 = axios.get('top-books');
-  const resultVal1 = await result1.then(data => data.data);
-  const dta = await resultVal1.map(e => e.books);
-  const booksTopFive = dta.map(e => e);
-  const markupForTopFive = markupTopFive(booksTopFive);
-  listOfBookFromCategory.innerHTML = await markupForTopFive;
-}
+// async function topFive() {
+//   const result1 = axios.get('top-books');
+//   const resultVal1 = await result1.then(data => data.data);
+//   const dta = await resultVal1.map(e => e.books);
+//   const booksTopFive = dta.map(e => e);
+//   const markupForTopFive = markupTopFive(booksTopFive);
+//   listOfBookFromCategory.innerHTML = await markupForTopFive;
+// }
 
-topFive();
+// topFive();
 
 // //---------------------------------------------------------------//
 
-const modal = document.querySelector('.modal');
+// const modal = document.querySelector('.modal');
 
-listOfBookFromCategory.addEventListener('click', e => {
-  const touch = e.target.closest('li');
-  const touchId = touch.dataset.id;
+// listOfBookFromCategory.addEventListener('click', e => {
+//   const touch = e.target.closest('li');
+//   const touchId = touch.dataset.id;
 
-  async function forModal() {
-    const result = axios.get(`${touchId}`);
-    const resultVal = await result.then(data => data.data);
-    const modalMake = makeModal(resultVal);
-    modal.innerHTML = modalMake;
-  }
+//   async function forModal() {
+//     const result = axios.get(`${touchId}`);
+//     const resultVal = await result.then(data => data.data);
+//     const modalMake = makeModal(resultVal);
+//     modal.innerHTML = modalMake;
+//   }
 
-  forModal();
-});
+//   forModal();
+// });
 
 // ============================================================ //
 
