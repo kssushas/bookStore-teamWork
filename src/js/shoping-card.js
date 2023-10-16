ba245c0872c52440aa27379c83785309cfde7646
Resonanse-91 <129589540+Resonanse-91@users.noreply.github.com>
2023-10-16 16:20:23 +0300

    shopping list

--- src/js/shoping-card.js
+++ src/js/shoping-card.js
@@ -36,3 +36,115 @@
 // }
 
 // topFive();
+
+import amazonI from '/src/images/icons/amazon.png';
+import ibookI from '/src/images/icons/ibook.png';
+import removeIcon from '/src/images/icons.svg';
+
+import bookShopI from '/src/images/icons/book-shop.png';
+import amazonWhite from '../images/icons/white-amazon.png';
+
+
+
+
+
+
+// Активна Shopping-кнопка на сторінці Shopping List
+
+document.querySelector('#shop-page').classList.add('active');
+document.querySelector('#bestsellers').classList.remove('active');
+
+
+
+// розмітка книг
+
+function generateBookCard(book) {
+  let amazonLnk = '';
+  let ibookLnk = '';
+  let bookshopLnk = '';
+  if (book.buy_links) {
+    for (const iterator of book.buy_links) {
+      switch (iterator.name) {
+        case 'Amazon':
+          amazonLnk = iterator.url;
+          break;
+        case 'Apple Books':
+          ibookLnk = iterator.url;
+          break;
+        case 'Bookshop':
+          const url = new URL(iterator.url);
+          bookshopLnk = url.searchParams.get('url1');
+          break;
+        default:
+          break;
+      }
+    }
+  }
+}
+function emptyShopping() {
+  return ` 
+    <p class="shop-list-text">
+      This page is empty, add some books and proceed to order.
+    </p>
+    <div class="shopping-list-book-img"></div>
+
+  `;
+}
+
+// Відображ. список книжок
+
+function renderBooks() {
+  const savedBooks = getSavedBooks();
+
+  if (savedBooks.length === 0) {
+    emptyListImg.style.display = 'block';
+      booksEl.innerHTML = '';
+      
+      // Приховує всі зображ, якщо список порож.
+      
+    imagesToHide.forEach(image => {
+      image.style.display = 'none';
+    });
+  } else {
+    const booksMarkup = savedBooks.map(book => generateBookCard(book)).join('');
+    booksEl.innerHTML = booksMarkup;
+    emptyListImg.style.display = 'none';
+    //  Відображ. зображ. якщо є книга в списку
+    imagesToHide.forEach(image => {
+      image.style.display = 'block';
+    });
+  }
+}
+
+
+containerBooks.addEventListener('click', removeButton);
+function removeButton(event) {
+  if (event.target.tagName != 'svg') return;
+  let curentBookId = event.target.parentElement.dataset.bookid;
+  if (!curentBookId) return;
+  const userBucket = load('userBucket');
+  const curentBook = [];
+  const userBucketNew = userBucket.filter(
+    iterator => iterator._id !== curentBookId
+  );
+  save('userBucket', userBucketNew);
+  showBooks();
+}
+
+const load = key => {
+  try {
+    const serializedState = localStorage.getItem(key);
+    return serializedState === null ? [] : JSON.parse(serializedState);
+  } catch (error) {
+    console.error('Get state error: ', error.message);
+    return [];
+  }
+};
+const save = (key, value) => {
+  try {
+    const serializedState = JSON.stringify(value);
+    localStorage.setItem(key, serializedState);
+  } catch (error) {
+    console.error('Set state error: ', error.message);
+  }
+};
