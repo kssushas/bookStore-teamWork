1ca173916ca5a1c928f07bb6a3d7c5515dcd6b72
Konstantin <aziimov1999123@gmail.com>
2023-10-16 15:42:14 +0300

    added and fixed scroller

--- src/js/anchor-btn.js
+++ src/js/anchor-btn.js
@@ -1,15 +1,12 @@
-function checkScrollPosition() {
-    const scrollToTopBtn = document.querySelector('#anchorBtn');
-    if (window.pageYOffset > 500) {
-      scrollToTopBtn.classList.add('show');
-    } else {
-      scrollToTopBtn.classList.remove('show');
-    }
+const scrollToTopBtn = document.querySelector('#anchorBtn');
+
+window.addEventListener('scroll', () => {
+  if (window.pageYOffset > 500) {
+    scrollToTopBtn.classList.add('show');
+  } else {
+    scrollToTopBtn.classList.remove('show');
   }
-  
-  function scrollToTop() {
-    window.scrollTo({ top: 0, behavior: 'smooth' });
-  }
-  
-  window.addEventListener('scroll', checkScrollPosition);
-  document.querySelector('#anchorBtn').addEventListener('click', scrollToTop);
+});
+scrollToTopBtn.addEventListener('click', () => {
+  window.scrollTo({ top: 0, behavior: 'smooth' });
+});
