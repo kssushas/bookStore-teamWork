6a1aa69e9979528ce30ab4810f58980e6455e404
Konstantin <aziimov1999123@gmail.com>
2023-10-16 15:29:07 +0300

    added scroller

--- src/js/anchor-btn.js
+++ src/js/anchor-btn.js
@@ -1,12 +1,15 @@
-// const scrollToTopBtn = document.querySelector('#anchorBtn');
-
-// window.addEventListener('scroll', () => {
-//   if (window.pageYOffset > 500) {
-//     scrollToTopBtn.classList.add('show');
-//   } else {
-//     scrollToTopBtn.classList.remove('show');
-//   }
-// });
-// scrollToTopBtn.addEventListener('click', () => {
-//   window.scrollTo({ top: 0, behavior: 'smooth' });
-// });
+function checkScrollPosition() {
+    const scrollToTopBtn = document.querySelector('#anchorBtn');
+    if (window.pageYOffset > 500) {
+      scrollToTopBtn.classList.add('show');
+    } else {
+      scrollToTopBtn.classList.remove('show');
+    }
+  }
+  
+  function scrollToTop() {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+  }
+  
+  window.addEventListener('scroll', checkScrollPosition);
+  document.querySelector('#anchorBtn').addEventListener('click', scrollToTop);
